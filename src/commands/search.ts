8f244699479e9da66665eca1701d1794064bfcbe
Mr Sharp <jms@marling.school>
2021-02-18 01:01:27 +0000

    using a playable embed for showing search algorithms at work

--- src/commands/search.ts
+++ src/commands/search.ts
@@ -1,9 +1,11 @@
 import { linearSearch, binarySearch } from "comp-sci-maths-lib";
 import { NO_MATCH } from 'comp-sci-maths-lib/dist/algorithms/search/common';
-import { SearchFunction } from "comp-sci-maths-lib/dist/types";
-import Discord from "discord.js";
-import { searchUtilitiesWithMsg } from "./common";
-import { MessageHandler } from "./types";
+import { SearchFunction, SearchUtilities } from "comp-sci-maths-lib/dist/types";
+import { Message, MessageEmbed, MessageReaction, User } from "discord.js";
+import { MessageHandler, SearchObservation } from "./types";
+import {
+    stringComparator,
+} from "comp-sci-maths-lib/dist/common";
 
 interface SearchHandlers {
     [s: string]: SearchFunction;
@@ -14,36 +16,112 @@ const searchHandlers: SearchHandlers = {
     'binary': binarySearch
 }
 
-const search: MessageHandler = (msg: Discord.Message, content: string, splitOnSpace: string[]) => {
-    if (splitOnSpace.length > 3) {
-        const [_, searchName, itemToFind, ...items] = splitOnSpace;
+const BACK_START = '⏪';
+const BACK_STEP = '◀️';
+const FORWARD_STEP = '▶️';
+const FORWARD_END = '⏩';
+const PLAY_CONTROLS = [BACK_START, BACK_STEP, FORWARD_STEP, FORWARD_END];
 
-        msg.channel.send(new Discord.MessageEmbed()
-            .setColor(0xff0000)
-            .setTitle(`Sorting using ${searchName}`)
-            .setDescription(`Searching for ${itemToFind} in ${items}`));
-
-        const searchHandler: SearchFunction = searchHandlers[searchName]
-        if (!!searchHandler) {
-            const found = searchHandler(items, itemToFind, searchUtilitiesWithMsg(msg));
-
-            if (found === NO_MATCH) {
-                msg.channel.send(new Discord.MessageEmbed()
-                    .setColor(0xff0000)
-                    .setTitle('Search Complete')
-                    .setDescription(`Could not find ${itemToFind} in ${items}`));
-            } else {
-                msg.channel.send(new Discord.MessageEmbed()
-                    .setColor(0x00ff00)
-                    .setTitle("Search Complete")
-                    .setDescription(`Found ${itemToFind} at ${found} in ${items}`));
-            }
-        } else {
-            msg.channel.send(`Unknown sorting algorithm ${searchName}, options are ${Object.keys(searchHandlers)}`)
-        }
-    } else {
-        msg.channel.send('Not enough parts to the command to sort')
+const createSearchObsMessage = (items: string[], itemToFind: string, observation: SearchObservation): MessageEmbed => {
+    const msg: MessageEmbed = new MessageEmbed()
+        .setColor(0x00ff00)
+        .setTitle('Searching')
+        .setDescription(observation.stageName)
+        .addField('Items', items.join(', '))
+        .addField('Item To Find', itemToFind)
+    if (Object.keys(observation.positionVars).length > 0) {
+        msg.addField('Position Variables', Object.entries(observation.positionVars).map(([name, value]) => `${name}=${value}`).join(' '));
     }
+    return msg;
+}
+
+const search: MessageHandler = async (msg: Message, content: string, splitOnSpace: string[]) => {
+    if (splitOnSpace.length <= 3) {
+        msg.channel.send('Not enough parts to the command to sort')
+        return;
+    }
+
+    const [_, searchName, itemToFind, ...items] = splitOnSpace;
+
+    const searchHandler: SearchFunction = searchHandlers[searchName];
+    if (!searchHandler) {
+        msg.channel.send(`Unknown sorting algorithm ${searchName}, options are ${Object.keys(searchHandlers)}`)
+        return;
+    }
+
+    const stages: SearchObservation[] = [];
+    let lastObservation: SearchObservation;
+
+    const searchUtilities: SearchUtilities<string> = {
+        compare: (a, b) => stringComparator(a, b),
+        observe: (stageName, positionVars) => {
+            lastObservation = {
+                stageName,
+                positionVars: { ...positionVars },
+            };
+            stages.push(lastObservation);
+        },
+    };
+
+    // Add explicit Start Observation
+    stages.push({
+        stageName: "Starting",
+        positionVars: {},
+    });
+
+    // Run the algorithm
+    let matchIndex: number = NO_MATCH;
+    matchIndex = searchHandler(
+        items,
+        itemToFind,
+        searchUtilities
+    );
+
+    // Add explicit End Observation
+    stages.push({
+        stageName: "Finished",
+        positionVars: {
+            matchIndex,
+        },
+    });
+
+    let observationIndex = 0;
+    const liveMessage = await msg.channel.send(createSearchObsMessage(items, itemToFind, stages[observationIndex]));
+
+    // Setup the play controls
+    PLAY_CONTROLS.forEach(async p => await liveMessage.react(p));
+
+    // Listen for those controls
+    const filter = (reaction: MessageReaction, user: User) => {
+        return PLAY_CONTROLS.includes(reaction.emoji.name) && user.id === msg.author.id;
+    };
+
+    const collector = liveMessage.createReactionCollector(filter, { time: 150000 });
+
+    collector.on('collect', (reaction: MessageReaction, user: User) => {
+        reaction.users.remove(user.id);
+
+        switch (reaction.emoji.name) {
+            case BACK_START:
+                observationIndex = 0;
+                break;
+            case BACK_STEP:
+                if (observationIndex > 0) {
+                    observationIndex -= 1;
+                }
+                break;
+            case FORWARD_STEP:
+                if (observationIndex < (stages.length - 1)) {
+                    observationIndex += 1;
+                }
+                break;
+            case FORWARD_END:
+                observationIndex = stages.length - 1;
+                break;
+        }
+
+        liveMessage.edit(createSearchObsMessage(items, itemToFind, stages[observationIndex]));
+    });
 }
 
 export default search;
--- src/commands/types.ts
+++ src/commands/types.ts
@@ -1,6 +1,17 @@
+import { PositionVars } from "comp-sci-maths-lib/dist/types";
 import Discord from "discord.js";
 
 export type MessageHandler = (msg: Discord.Message, content: string, splitOnSpace: string[]) => void;
 export interface MessageHandlers {
     [s: string]: MessageHandler;
 }
+
+export interface SearchObservation {
+    stageName: string;
+    positionVars: PositionVars;
+}
+
+export const DEFAULT_SEARCH_OBS: SearchObservation = {
+    stageName: "DEFAULT",
+    positionVars: {},
+};
